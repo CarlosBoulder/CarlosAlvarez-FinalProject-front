62e48d9bb0e2c783d432e60dc3c257a009c62b21
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-30 13:18:41 +0200

    Add LoginPageStyled

---
+++ src/pages/LoginPage/LoginPageStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const LoginPageStyled = styled.section`
+  padding: 12px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export default LoginPageStyled;
