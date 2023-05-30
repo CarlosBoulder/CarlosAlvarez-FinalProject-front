d7621af0a333fa69551486616d76bb267b7f970b
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-30 12:29:50 +0200

    Add HeaderStyled

---
+++ src/components/Header/HeaderStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  padding: 12px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-direction: row;
+
+  .header-logo {
+    position: absolute;
+  }
+`;
+
+export default HeaderStyled;
