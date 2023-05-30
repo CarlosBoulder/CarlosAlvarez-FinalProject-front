b3992fb4e84f60f39b517a8122b716e4b29bc531
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-30 10:45:54 +0200

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = (): React.ReactElement => {
+  return (
+    <img
+      src="images/logoBoulderLab.svg"
+      alt="boulderlab logo"
+      width={184}
+      height={86}
+    />
+  );
+};
+
+export default Header;
