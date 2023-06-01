ca45f3e9980fa168c7408131344d63dc45f02e27
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-01 17:08:35 +0200

    Add useUser test

---
+++ src/hooks/useUser/useUser.test.ts
@@ -0,0 +1,25 @@
+import { renderHook } from "@testing-library/react";
+import useUser, { UserCredentials } from "./useUser";
+
+describe("Given a useUser custom hook", () => {
+  describe("When calls getToken function with a valid username and password", () => {
+    test("Then it should return a user token", async () => {
+      const mockUserCredentials: UserCredentials = {
+        username: "test",
+        password: "test",
+      };
+
+      const tokenMock = "rsdtfvgbhnjmkl";
+
+      const {
+        result: {
+          current: { getToken },
+        },
+      } = renderHook(() => useUser(mockUserCredentials));
+
+      const token = await getToken();
+
+      expect(token).toBe(tokenMock);
+    });
+  });
+});
