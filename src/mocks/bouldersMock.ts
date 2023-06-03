d00763ba2b4700df0fc4573b9a79aee24074f81f
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-03 22:18:31 +0200

    Add loadBouldersReducer test

---
+++ src/mocks/bouldersMock.ts
@@ -0,0 +1,15 @@
+import BoulderStructure from "../store/types";
+
+const bouldersMock: BoulderStructure[] = [
+  {
+    img: "imgUrl",
+    name: "Rema remero",
+    crag: "Techos",
+    spot: "Albarracín",
+    country: "España",
+    description: "Sit start from small crimps",
+    grade: "7A",
+  },
+];
+
+export default bouldersMock;
---
+++ src/store/boulder/loadBouldersReducer.ts
@@ -0,0 +1,32 @@
+import bouldersMock from "../../mocks/bouldersMock";
+import BoulderStructure from "../types";
+import {
+  BoulderState,
+  loadBouldersActionCreator,
+  loadBouldersReducer,
+} from "./boulderSlice";
+
+describe("Given a loadBoulders reducer", () => {
+  describe("When it receives an empty list of boulders and a loadBoulders action with one boulder", () => {
+    test("Then it should return a list with one boulder", () => {
+      const currentState: BoulderState = {
+        boulders: [],
+      };
+
+      const boulders: BoulderStructure[] = bouldersMock;
+
+      const expectedBouldersState: BoulderState = {
+        boulders: boulders,
+      };
+
+      const loadBouldersAction = loadBouldersActionCreator(boulders);
+
+      const newBoulderState = loadBouldersReducer(
+        currentState,
+        loadBouldersAction
+      );
+
+      expect(newBoulderState).toStrictEqual(expectedBouldersState);
+    });
+  });
+});
