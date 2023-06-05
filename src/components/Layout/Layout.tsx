3139ee08afef641ca615a532c8543d97fd44fe7c
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-05 10:24:33 +0200

    Add loader component and loaderStyled

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import React from "react";
 import ContainerStyled from "../shared/ContainerStyled";
+import Loader from "../Loading/Loader";
+
 const Layout = (): React.ReactElement => {
   return (
     <>
       <ContainerStyled>
         <Header />
         <Outlet />
+        <Loader />
       </ContainerStyled>
     </>
   );
