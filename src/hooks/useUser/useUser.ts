1ce1fd2b3d6935ffaa25477ad723638da750c59f
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-05 17:25:29 +0200

    Add  try and catch to useUser

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -9,8 +9,16 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 const useUser = () => {
   const getToken = async (credential: UserCredentials): Promise<string> => {
-    const { data: data } = await axios.post(`${apiUrl}/user/login`, credential);
-    return data.token;
+    try {
+      const { data: data } = await axios.post(
+        `${apiUrl}/user/login`,
+        credential
+      );
+
+      return data.token;
+    } catch (error) {
+      throw new Error("Wrong Credentials");
+    }
   };
 
   return { getToken };
