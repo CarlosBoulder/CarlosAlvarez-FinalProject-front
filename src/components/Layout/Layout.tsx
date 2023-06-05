a1e91907f4d080b85d1ee18aaea32cec4eb300d7
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-05 13:43:03 +0200

    Add show and hide loading to useBoulder hook and fix its test

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import React from "react";
 import ContainerStyled from "../shared/ContainerStyled";
+import { useAppSelector } from "../../store";
+import Loader from "../Loading/Loader";
 
 const Layout = (): React.ReactElement => {
+  const isLoading = useAppSelector((state) => state.uiStore.isLoading);
+
   return (
     <>
       <ContainerStyled>
         <Header />
+        {isLoading && <Loader />}
         <Outlet />
       </ContainerStyled>
     </>
