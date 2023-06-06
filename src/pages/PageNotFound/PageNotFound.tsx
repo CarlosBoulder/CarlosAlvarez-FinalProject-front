29ae8a756b45efae29c155e9139196beb107d124
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-06 21:27:17 +0200

    Add PageNotFound component

---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import PageNotFoundStyled from "./PageNotFoundStyled";
+
+const PageNotFound = (): React.ReactElement => {
+  return (
+    <PageNotFoundStyled>
+      <section>
+        <h2>Page Not Found</h2>
+      </section>
+    </PageNotFoundStyled>
+  );
+};
+
+export default PageNotFound;
---
+++ src/pages/PageNotFound/PageNotFoundStyled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const PageNotFoundStyled = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  font-family: "Montserrat";
+  font-size: 2rem;
+  padding: 0;
+`;
+
+export default PageNotFoundStyled;
