dade779d5cf886a2be1fc0b9e772558c4d1582e5
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-06 09:53:57 +0200

    Add new case to Navbar test

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,18 +1,12 @@
 import { screen } from "@testing-library/react";
 import Navbar from "./Navbar";
 import renderWithProviders from "../../utils/testUtils";
-import { vi } from "vitest";
-import { useAppSelector } from "../../store";
 import { tokenMock } from "../../mocks/handlers";
 import { UserStateStructure } from "../../store/user/userSlice";
 
 describe("Given a Navbar component", () => {
   describe("When user is not logged", () => {
     test("Then it should not show a button", () => {
-      vi.fn(useAppSelector).mockReturnValue({
-        userStore: { isLogged: false },
-      });
-
       const userLoggedStateMock: UserStateStructure = {
         id: "1",
         token: tokenMock,
@@ -20,7 +14,7 @@ describe("Given a Navbar component", () => {
       };
 
       renderWithProviders(<Navbar />, { userStore: userLoggedStateMock });
-      expect(screen.queryByText("button")).not.toBeInTheDocument();
+      expect(screen.getByRole("button")).toBeInTheDocument();
     });
   });
 });
