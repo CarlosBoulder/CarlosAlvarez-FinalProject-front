dfdc1e7fb8a9d78db4d91dae8164e340f33f419e
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-09 11:48:48 +0200

    Fix BoulderCard test

--- src/components/BoulderCard/BoulderCard.test.tsx
+++ src/components/BoulderCard/BoulderCard.test.tsx
@@ -3,13 +3,18 @@ import bouldersMock from "../../mocks/bouldersMock";
 import BoulderStructure from "../../store/types";
 import renderWithProviders from "../../utils/testUtils";
 import BoulderCard from "./BoulderCard";
+import { vi } from "vitest";
 
 describe("Given a BoulderCard component", () => {
   describe("When it is rendered with the boulder 'Rema remero'", () => {
     test("Then it should show a heading with the name 'Rema remero'", () => {
       const boulder: BoulderStructure = bouldersMock[0];
 
-      renderWithProviders(<BoulderCard boulder={boulder} />);
+      const actionOnClick = vi.fn();
+
+      renderWithProviders(
+        <BoulderCard boulder={boulder} actionOnClick={actionOnClick} />
+      );
 
       const expectedName = screen.getByRole("heading", {
         name: `${bouldersMock[0].name} ${bouldersMock[0].grade}`,
