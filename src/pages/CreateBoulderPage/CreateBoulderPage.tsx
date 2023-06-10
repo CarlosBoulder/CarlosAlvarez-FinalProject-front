b3aaf8c65f2d29cf520caf8995426dd1f337c800
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-10 13:08:33 +0200

    Add CreateBoulderPage

---
+++ src/pages/CreateBoulderPage/CreateBoulderPage.tsx
@@ -0,0 +1,13 @@
+import CreatePageStyled from "./CreateBoulderPageStyled";
+
+const CreateBoulderPage = (): React.ReactElement => {
+  return (
+    <CreatePageStyled>
+      <section>
+        <h2>Create your boulder</h2>
+      </section>
+    </CreatePageStyled>
+  );
+};
+
+export default CreateBoulderPage;
---
+++ src/pages/CreateBoulderPage/CreateBoulderPageStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const CreateBoulderPagePageStyled = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  font-family: "Montserrat";
+  font-size: 1.5rem;
+  padding: 0;
+`;
+
+export default CreateBoulderPagePageStyled;
