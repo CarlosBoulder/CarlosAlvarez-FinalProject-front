c4658a277f0eb86e69d85a2e12601c5a278f0b8f
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-11 11:40:07 +0200

    Add onSubmit to CreateBourderForm

--- src/pages/CreateBoulderPage/CreateBoulderPage.tsx
+++ src/pages/CreateBoulderPage/CreateBoulderPage.tsx
@@ -1,12 +1,26 @@
 import CreateBoulderForm from "../../components/CreateBoulderForm/CreateBoulderForm";
+import useBoulders from "../../hooks/useBoulders/useBoulders";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../store";
 import CreatePageStyled from "./CreateBoulderPageStyled";
+import { BoulderStructureDetails } from "../../components/CreateBoulderForm/types";
 
 const CreateBoulderPage = (): React.ReactElement => {
+  const token = useAppSelector((store) => store.userStore.token);
+  const navigate = useNavigate();
+
+  const { addBoulder } = useBoulders(token);
+
+  const handleOnSubmit = async (boulder: BoulderStructureDetails) => {
+    await addBoulder(boulder);
+    navigate("/home", { replace: true });
+  };
+
   return (
     <CreatePageStyled>
       <section>
         <h2>Create your boulder</h2>
-        <CreateBoulderForm />
+        <CreateBoulderForm handleOnSubmit={handleOnSubmit} />
       </section>
     </CreatePageStyled>
   );
