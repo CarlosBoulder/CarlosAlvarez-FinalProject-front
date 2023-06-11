9ea445c3eeb71038a3150f1c8f7a9662bdd0f90d
Carlos ALvarez <carlosmail1978@gmail.com>
2023-06-11 18:50:25 +0200

    Change BoulderList width and Loader background

--- src/components/BoulderList/BoulderList.tsx
+++ src/components/BoulderList/BoulderList.tsx
@@ -2,6 +2,7 @@ import useBoulders from "../../hooks/useBoulders/useBoulders";
 import { useAppSelector } from "../../store";
 import BoulderStructure from "../../store/types";
 import BoulderCard from "../BoulderCard/BoulderCard";
+import BoulderListStyled from "./BoulderListStyled";
 
 export interface BoulderListProps {
   boulders: BoulderStructure[];
@@ -16,13 +17,13 @@ const BoulderList = ({ boulders }: BoulderListProps): React.ReactElement => {
   };
 
   return (
-    <ul className="boulder-list">
+    <BoulderListStyled className="boulder-list">
       {boulders.map((boulder) => (
         <li key={boulder.id}>
           <BoulderCard boulder={boulder} actionOnClick={deleteOnClick} />
         </li>
       ))}
-    </ul>
+    </BoulderListStyled>
   );
 };
 
